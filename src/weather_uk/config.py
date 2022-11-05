1669836e035c7e76c41b6729fc0a4aa0c5a3afa8
TomJGooding <101601846+TomJGooding@users.noreply.github.com>
2022-11-05 17:37:43 +0000

    feat(config): add UserConfig dataclass

--- src/weather_uk/config.py
+++ src/weather_uk/config.py
@@ -1,5 +1,6 @@
 import shutil
 from configparser import ConfigParser
+from dataclasses import dataclass
 from pathlib import Path
 
 import platformdirs
@@ -11,6 +12,28 @@ CONFIG_PATH: Path = platformdirs.user_config_path(APPNAME)
 CONFIG_FILENAME = f"{APPNAME}.cfg"
 
 CONFIG_TEMPLATE = files("weather_uk.config_template").joinpath(CONFIG_FILENAME)
+
+
+@dataclass
+class UserConfig:
+    apikey: str
+
+    @classmethod
+    def from_cfg_file(cls, config_path: Path = CONFIG_PATH):
+        cfgparser = ConfigParser()
+        config_file = config_path / CONFIG_FILENAME
+        cfgparser.read(config_file)
+
+        try:
+            auth = cfgparser["auth"]
+            apikey = auth["apikey"]
+            return cls(apikey=apikey)
+        except KeyError as e:
+            msg = (
+                f"Error detected in config file: {config_file}  "
+                f"Missing expected {e} entry."
+            )
+            print(msg)
 
 
 def setup_config_if_none_exists(config_path: Path = CONFIG_PATH) -> None:
