1b6889533fcd7982c84e4cac7a9fcae85e7a87b2
TomJGooding <101601846+TomJGooding@users.noreply.github.com>
2022-11-19 16:11:28 +0000

    feat(weather): shorten weather strings

--- tests/test_weather_type.py
+++ tests/test_weather_type.py
@@ -10,4 +10,16 @@ def test_weather_type_sunny_day():
 def test_weather_type_thunder_day():
     weather_type_code = 29
     weather_type = WeatherType(weather_type_code)
-    assert str(weather_type) == "Thunder shower (day)"
+    assert str(weather_type) == "Thunder shower"
+
+
+def test_weather_type_clear_night():
+    weather_type_code = 0
+    weather_type = WeatherType(weather_type_code)
+    assert str(weather_type) == "Clear night"
+
+
+def test_weather_type_thunder_night():
+    weather_type_code = 28
+    weather_type = WeatherType(weather_type_code)
+    assert str(weather_type) == "Thunder shower"
